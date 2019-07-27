5dd7ed7e07cddf60780c946346688cdbac136827
hulunbao <hulunbao@gmail.com>
2019-07-27 11:10:11 +0800

    problem03

--- problem03/problem03.go
+++ problem03/problem03.go
@@ -4,7 +4,7 @@ func Find(array [][]int, value int) bool{
 	rows := len(array)
 	columns := len(array[0])
 
-	for r, c := 0,columns - 1; r > rows && c <= columns; {
+	for r, c := 0,columns - 1; r < rows && c >= 0; {
 		if array[r][c] == value{
 			return true
 		}
--- problem03/problem03_test.go
+++ problem03/problem03_test.go
@@ -1,7 +1,61 @@
 package problem03
 
-import "testing"
+import (
+	"testing"
+)
+
+type para struct {
+	array [][]int
+	value int
+}
+
+type ans struct {
+	find bool
+}
+
+type question struct {
+	p para
+	a ans
+}
 
 func TestFind(t *testing.T) {
+	question := []question{
+		{
+			p: para{
+				array: [][]int{
+					[]int{1,2,8,9},
+					[]int{2,4,9,12},
+					[]int{4,7,10,13},
+					[]int{6,8,11,15},
+				},
+				value: 7,
+			},
+			a: ans{
+				find:true,
+			},
+		},
+		{
+			p:para{
+				array: [][]int{
+					[]int{ 1, 2, 8, 9, },
+					[]int{ 2, 4, 9, 12, },
+					[]int{ 4, 6, 10, 13, },
+					[]int{ 6, 8, 11, 15, },
+				},
+				value:7,
+			},
+			a:ans{
+				find:false,
+			},
+		},
 
+	}
+
+	for _, q := range question{
+		a, p := q.a, q.p
+		if a.find != Find(p.array,p.value){
+			t.Errorf("expected answer is %v,but is %v",a.find,Find(p.array,p.value))
+		}
+
+	}
 }
